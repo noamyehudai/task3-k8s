81014825bb8044cde7d727df7e5fbb81257e1174
EC2 Default User <ec2-user@ip-10-129-8-107.eu-central-1.compute.internal>
2020-04-02 12:19:25 +0000

    added installation file

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Build webapp image
+docker build -t noamyehudai/task3-image:latest .
+
+# Push the image to repository
+docker push noamyehudai/task3-image:latest
+
+# Run the cluster using Helm
+helm install task3 ./kubernetes/
